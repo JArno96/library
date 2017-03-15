e138a1d2471fb31ff7c92b80bf721f1589241ff1
JArno96 <b5021487@my.shu.ac.uk>
2017-03-15 10:51:06 +0000

    I have now made my Librarian and Library domain class and controller

---
+++ librarySystem/grails-app/controllers/com/jarno/LibraryController.groovy
@@ -0,0 +1,6 @@
+package com.jarno
+
+class LibraryController {
+
+    def scaffold=Library
+}
---
+++ librarySystem/grails-app/domain/com/jarno/Library.groovy
@@ -0,0 +1,12 @@
+package com.jarno
+
+class Library {
+String name
+String address
+String openingHours
+String location
+String studySpaces
+
+    static constraints = {
+    }
+}
